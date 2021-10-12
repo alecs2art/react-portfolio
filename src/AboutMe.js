3fd2e064387cf1ac5563ba521380b8fcd31e4ccc
unknown <alecs2art@gmail.com>
2021-10-12 13:03:51 -0700

    created header and about me section

---
+++ src/AboutMe.js
@@ -0,0 +1,15 @@
+const AboutMe = () => {
+    return (
+        <div>
+            <section id="a" class="about-me">
+                <h2>About Me</h2>
+                <p>
+                    Here is some placeholder text while I come up with a way to describe myself.
+                </p>
+
+            </section>
+        </div>
+    )
+}
+
+export default AboutMe
--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>
+            Alec Stewart's Portfolio
+        </h1>
+<nav pageRender = {setPagerender} />
       </header>
+      <div>
+        <p>
+          Cool Subtitle Here!
+        </p>
+      </div>
+       {pageRender === "About Me"?<AboutMe />:""}
+      {pageRender === "Projects"?<Projects />:""}
+      {pageRender === "Contact Me"?<Contact />:""}
     </div>
   );
 }
