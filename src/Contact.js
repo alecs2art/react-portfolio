fbd888afa17d05a21872143bf35d17694d532734
unknown <alecs2art@gmail.com>
2021-10-12 13:35:57 -0700

    added projects and contact components

---
+++ src/Contact.js
@@ -0,0 +1,27 @@
+const Contact = () => {
+    return (
+        <div>
+            <section id="c" class="contact-me">
+                <h2>Contact Me</h2>
+
+                <ul>
+                    <li>
+                        904.303.0606
+                    </li>
+                    <li>
+                        alecs2art@gmail.com
+                    </li>
+                    <li>
+                        Github
+                    </li>
+                    <li>
+                        Twitter
+                    </li>
+                </ul>
+        
+            </section>
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/Projects.js
@@ -0,0 +1,51 @@
+const Projects = () => {
+    return (
+        <div>
+            <section id="w" class="work">
+        <h2>Work</h2>
+
+        <div class="flex-box">
+
+            <div class="caption-king">
+                <article>
+                    <h3>Caption King</h3>
+                    <p>Full Stack</p>
+                </article>    
+            </div>
+
+            <div class="led-wall">
+                <article>
+                    <h3>LED Wall</h3>
+                    <p>Node/joT</p>
+                </article>    
+            </div>
+
+            <div class="calculator">
+                <article>
+                    <h3>Calculator</h3>
+                    <p>React/JavaScript/CSS</p>
+                </article>
+            </div>
+
+            <div class="pastel-puzzels">
+                <article>
+                    <h3>Pastel Puzzels</h3>
+                    <p>MERN Stack</p>
+                </article>
+            </div>
+
+            <div class="run-buddy">
+                <article>
+                    <h3>Run Buddy</h3>
+                    <p>HTML/CSS</p>
+                </article>
+            </div>
+
+        </div>    
+
+    </section>
+        </div>
+    )
+}
+
+export default Projects
